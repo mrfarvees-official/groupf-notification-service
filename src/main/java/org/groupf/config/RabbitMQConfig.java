3fd85473f33a947ca672f59527827379e69a3de4
mrfarvees-official <saroathfarvees@gmail.com>
2026-06-25 14:39:59 +0530

    Added RabbitMQ and Docker container

--- src/main/java/org/groupf/config/RabbitMQConfig.java
+++ src/main/java/org/groupf/config/RabbitMQConfig.java
@@ -1,11 +1,40 @@
 package org.groupf.config;
 
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+
+    @Value("${rabbitmq.exchange.order}")
+    private String orderExchange;
+
+    @Value("${rabbitmq.queue.notification-order}")
+    private String notificationOrderQueue;
+
+    @Value("${rabbitmq.routing-key.order-created}")
+    private String orderCreatedRoutingKey;
+
+    @Bean
+    public TopicExchange orderExchange() {
+        return new TopicExchange(orderExchange);
+    }
+
+    @Bean
+    public Queue notificationOrderQueue() {
+        return new Queue(notificationOrderQueue, true);
+    }
+
+    @Bean
+    public Binding notificationOrderBinding() {
+        return BindingBuilder
+                .bind(notificationOrderQueue())
+                .to(orderExchange())
+                .with(orderCreatedRoutingKey);
+    }
 
     @Bean
     public Jackson2JsonMessageConverter jsonMessageConverter() {
--- src/main/java/org/groupf/listener/OrderNotificationListener.java
+++ src/main/java/org/groupf/listener/OrderNotificationListener.java
@@ -2,16 +2,12 @@ package org.groupf.listener;
 
 import lombok.extern.slf4j.Slf4j;
 import org.groupf.dto.OrderCreatedEvent;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class OrderNotificationListener {
-
-    private static final Logger log = LoggerFactory.getLogger(OrderNotificationListener.class);
 
     @RabbitListener(queues = "${rabbitmq.queue.notification-order}")
     public void receiveOrderCreatedEvent(OrderCreatedEvent event) {
@@ -25,7 +21,5 @@ public class OrderNotificationListener {
                 event.quantity(),
                 event.status()
         );
-
-
     }
 }
